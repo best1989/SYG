0fb237091c1e2224af1166adedf6c51ecccf9d67
best1989 <gabguzmanro@gmail.com>
2015-10-25 20:37:35 -0430

    Intento para cargar la imagen...

--- app/src/main/java/com/lapropuesta/paraloquetal/syg/Home.java
+++ app/src/main/java/com/lapropuesta/paraloquetal/syg/Home.java
@@ -1,7 +1,21 @@
 package com.lapropuesta.paraloquetal.syg;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 /**
  * Created by gguzman on 10/19/15.
@@ -13,6 +27,38 @@ public class Home extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("_User");
+        //query.fromLocalDatastore();
+        // query.whereEqualTo("playerName", "Dan Stemkoski");
 
+        query.getInBackground("72AJ7gyRc5", new GetCallback<ParseObject>() {
+
+            public void done(ParseObject object, ParseException e) {
+                // Locate the column named "ImageName" and set
+                // the string
+                Toast.makeText(getBaseContext(),object.get("username").toString()+object.get("email").toString(),Toast.LENGTH_LONG).show();
+                ParseFile fileObject = (ParseFile) object.get("usrimg");
+
+
+//                fileObject.getDataInBackground(new GetDataCallback() {
+//                    public void done(byte[] data, ParseException e) {
+//                        if (e == null) {
+//                            Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+//                            // Get the ImageView from
+//                            // main.xml
+//                            ImageView image = (ImageView) findViewById(R.id.profile_picture);
+//                            // Set the Bitmap into the
+//                            // ImageView
+//                            image.setImageBitmap(bmp);
+//                        } else {
+//                            ImageView image = (ImageView) findViewById(R.id.profile_picture);
+//                            image.setImageDrawable(getDrawable(R.drawable.sg));
+//                            //  Log.d("score", "Error: " + e.getMessage());
+//                        }
+//                    }
+//
+//                });
+            }
+        });
     }
 }
--- app/src/main/java/com/lapropuesta/paraloquetal/syg/LoginActivity.java
+++ app/src/main/java/com/lapropuesta/paraloquetal/syg/LoginActivity.java
@@ -120,8 +120,8 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
             focusView = mPasswordView;
             cancel = true;
         }
@@ -131,11 +131,11 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             mEmailView.setError(getString(R.string.error_field_required));
             focusView = mEmailView;
             cancel = true;
-        } else if (!isEmailValid(email)) {
+        }/* else if (!isEmailValid(email)) {
             mEmailView.setError(getString(R.string.error_invalid_email));
             focusView = mEmailView;
             cancel = true;
-        }
+        }*/
 
         if (cancel) {
             // There was an error; don't attempt login and focus the first
@@ -150,16 +150,16 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         }
     }
 
-    private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return true;
-        //email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
-    }
+//    private boolean isEmailValid(String email) {
+//        //TODO: Replace this with your own logic
+//        return true;
+//        //email.contains("@");
+//    }
+//
+//    private boolean isPasswordValid(String password) {
+//        //TODO: Replace this with your own logic
+//        return password.length() > 4;
+//    }
 
     /**
      * Shows the progress UI and hides the login form.
@@ -170,7 +170,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         // for very easy animations. If available, use these APIs to fade-in
         // the progress spinner.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_longAnimTime);
 
             mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
             mLoginFormView.animate().setDuration(shortAnimTime).alpha(
